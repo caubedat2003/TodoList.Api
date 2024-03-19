48fbafad51fc95f01271b1013f371454b7c1f821
Anh Dat <caubedatt2003@gmail.com>
2024-03-19 14:39:05 +0700

    commit init

---
+++ TodoList.Api/Data/TodoListDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TodoList.Api.Entities;
+using Task = TodoList.Api.Entities.Task;
+
+namespace TodoList.Api.Data
+{
+    public class TodoListDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public TodoListDbContext(DbContextOptions<TodoListDbContext> options) : base(options) {
+        
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ TodoList.Api/Entities/Task.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TodoList.Api.Enums;
+
+namespace TodoList.Api.Entities
+{
+    public class Task
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public Guid? AssigneeId { get; set; }
+        [ForeignKey("AssigneeId")]
+        public User Assignee {  get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Priority Priority { get; set;}
+        public Status Status { get; set; }
+    }
+}
